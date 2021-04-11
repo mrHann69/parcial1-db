f110e17a4af0ac7894b59eea5432a4fc67bd0f2b
tsubakijuju13 <santiago.ramirez.ospina@correounivalle.edu.co>
2021-04-11 19:19:18 -0400

    New version queries

--- solucion_parcial_1.sql
+++ solucion_parcial_1.sql
@@ -5,28 +5,11 @@
 */
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*
+ * Punto 1
+ * El salario promedio de los profesores de la facultad de Salud
+ */
+ select avg(SUELDO) as salario_promedio from PROFE where cod_facultad='sal';
 
 
 --punto 2
@@ -36,6 +19,29 @@ from asignatura a, programacion p
 where a.plan_id = 3 and p.periodo = '2014-1';
 
 
+/*
+ * Punto 3
+ * Código y nombre de asignaturas que han dictado los profesores de categoría 3 y categoría 4.
+ */
+ (select a.COD_A, NOM_ASIG from ASIGNATURA a, programacion p where COD_P in (select COD_P from profe where CODCAT='3') and
+                                                            a.COD_A=p.COD_A group by a.COD_A, NOM_ASIG)
+intersect
+(select a.COD_A, NOM_ASIG from ASIGNATURA a, programacion p where COD_P in (select COD_P from profe where CODCAT='4') and
+                                                            a.COD_A=p.COD_A group by a.COD_A, NOM_ASIG);
+
+/*
+ * Punto 4
+ * jujujujujujuju
+ */
+
+
+/*
+ * Punto 5
+ * Código y nombre de asignaturas que nunca se han programado
+ */
+(select COD_A, NOM_ASIG from ASIGNATURA)
+except
+(select p.COD_A, NOM_ASIG from PROGRAMACION p, ASIGNATURA a  where p.COD_A=a.COD_A);
 
 
 -- punto 6
@@ -46,12 +52,34 @@ from profe p,asignatura a, programacion p2
 where a.creditos > 3 and p2.periodo ='2013-1';
 
 
+/*
+ * Punto 7
+ * Código y nombre de asignaturas que han dado profesores de categoría 1 o categoría 2
+ */
+select p.COD_A, NOM_ASIG from PROGRAMACION p, ASIGNATURA a
+where COD_P in (select COD_P from PROFE where CODCAT in ('1', '2')) and
+      p.COD_A=a.COD_A group by p.COD_A, NOM_ASIG;
+
+
 -- punto 8
 --Código y nombre de asignaturas que han dado profesores de categoría 2 o categoría 3 pero no los de la categoría 1
 select distinct a.cod_a , a.nom_asig, p2.codcat
 from asignatura a, programacion p, profe p2
 where p2.codcat != '1'
 order by p2.codcat ;
+
+
+/*
+ * Punto 9
+ * Código y Nombre de profesores de la facultad de Ingeniería que solo han dado clase en el periodo 2017-1
+ */
+(select p.COD_P, NOM_PROF from programacion p, profe pr where PERIODO='2017-1'
+                                                          and cod_facultad='ing'
+                                                          and p.COD_P=pr.COD_P group by p.COD_P, NOM_PROF)
+except
+(select p.COD_P, NOM_PROF from programacion p, profe pr where PERIODO in (select PERIODO from programacion where PERIODO<>'2017-1')
+                                                and cod_facultad='ing'
+                                                and p.cod_p=pr.COD_P group by p.COD_P, NOM_PROF);
 
 -- Punto 10
 --Código y Nombre de Profesores de la facultad de Ingeniería que dieron alguna asignatura de la facultad de salud
